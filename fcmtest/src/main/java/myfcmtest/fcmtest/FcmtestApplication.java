f4d6d4a97c4a0d4a37b265280c2a058a9def3ea7
CindoddCindy <cindodcindy@gmail.com>
2021-01-22 11:14:32 +0700

    masih ga ngerti konsepnya

---
+++ fcmtest/src/main/java/myfcmtest/fcmtest/FcmtestApplication.java
@@ -0,0 +1,13 @@
+package myfcmtest.fcmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcmtestApplication.class, args);
+	}
+
+}
---
+++ fcmtest/src/test/java/myfcmtest/fcmtest/FcmtestApplicationTests.java
@@ -0,0 +1,13 @@
+package myfcmtest.fcmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
